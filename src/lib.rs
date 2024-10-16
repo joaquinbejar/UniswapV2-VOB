04cab16c0c0e7b3a4c344f23446d140f896e310a
Joaquin Bejar <jb@taunais.com>
2024-10-16 19:07:35 +0200

    Add new modules for analytics, API, events, orderbook, and web3
    Created foundational modules including analytics, API, constants, events, orderbook, utils, and web3. Each module contains necessary sub-modules to support initial structure and functionality.

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/******************************************************************************
+   Author: Joaquín Béjar García
+   Email: jb@taunais.com
+   Date: 1/8/24
+******************************************************************************/
+extern crate core;
+
+pub mod analytics;
+pub mod api;
+pub mod constants;
+pub mod events;
+pub mod orderbook;
+pub mod utils;
+pub mod web3;
---
+++ src/orderbook/mod.rs
@@ -0,0 +1,9 @@
+/******************************************************************************
+   Author: Joaquín Béjar García
+   Email: jb@taunais.com
+   Date: 7/10/24
+******************************************************************************/
+pub mod event_processor;
+pub mod model;
+
+pub(crate) mod logic;
