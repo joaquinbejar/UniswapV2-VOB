6b78fdffb27dcf51898ae26f7964e42dc8e2d75d
Joaquin Bejar <jb@taunais.com>
2024-10-16 19:08:47 +0200

    Add constants for ETH-USDT pair and blockchain interaction
    Introduce a new file `constants.rs` to store important constants such as ETH-USDT pair address, event signatures, and INFURA URL. This will centralize configuration and make the codebase more maintainable.

---
+++ src/constants.rs
@@ -0,0 +1,17 @@
+/******************************************************************************
+   Author: Joaquín Béjar García
+   Email: jb@taunais.com
+   Date: 11/8/24
+******************************************************************************/
+
+pub const ETH_USDT_PAIR_ADDRESS: &str = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852";
+
+pub const SWAP_EVENT_SIGNATURE: &str = "Swap(address,address,uint256,uint256,uint256,uint256)";
+
+pub const INFURA_URL: &str = "wss://mainnet.infura.io/ws/v3/";
+
+pub const ETH_DECIMALS: u8 = 18;
+
+pub const USDT_DECIMALS: u8 = 6;
+
+pub const VOB_DEPTH: usize = 10; // Virtual Order Book depth
